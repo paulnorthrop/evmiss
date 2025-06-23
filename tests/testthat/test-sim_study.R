3f13b118e45a538e5e47ca3e97ada31a890ad89b
Paul Northrop <p.northrop@ucl.ac.uk>
2025-06-23 03:41:50 +0100

    Add test

--- tests/testthat/test-sim_study.R
+++ tests/testthat/test-sim_study.R
@@ -45,14 +45,35 @@ test_that("sim_study(): user-supplied statistics function (GEV parameters)", {
 
 # Check that summary() and tab() produce consistent results
 
+# Return levels
+
+res1m <- attr(res1, "matrix")
+res1bias <- res1m[1:3, ]
+res1sd <- res1m[4:6, ]
+res1rmse <- res1m[7:9, ]
+res1med <- res1m[10:12, ]
+res1qir <- res1m[13:15, ]
+res1mae <- res1m[16:18, ]
+res1all <- cbind(res1bias, res1med, res1sd, res1qir, res1rmse, res1mae)
+tab1 <- as.matrix(tab(res, return_period = c(100, 1000)))
+# Extract only
+test_that("sim_study(): summary() vs tab()", {
+  testthat::expect_equal(res1all,
+                         as.matrix(tab(res, return_period = c(100, 1000))),
+                         ignore_attr = TRUE)
+})
+
+# GEV estimates
+
 res3m <- attr(res3, "matrix")
 res3bias <- res3m[1:3, ]
 res3sd <- res3m[4:6, ]
 res3rmse <- res3m[7:9, ]
 res3all <- cbind(res3bias, res3sd, res3rmse)
+tab3 <- as.matrix(tab(res))
 
 test_that("sim_study(): summary() vs tab()", {
-  testthat::expect_equal(res3all, as.matrix(tab(res)), ignore_attr = TRUE)
+  testthat::expect_equal(res3all, tab3, ignore_attr = TRUE)
 })
 
 
