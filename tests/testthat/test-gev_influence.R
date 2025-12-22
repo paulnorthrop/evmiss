3791f6cc9226547609d8878bd228f12f1ee33dc5
Paul Northrop <p.northrop@ucl.ac.uk>
2025-12-22 22:28:28 +0000

    Add test

--- tests/testthat/test-gev_influence.R
+++ tests/testthat/test-gev_influence.R
@@ -12,6 +12,17 @@ inf2 <- gev_influence(z = z, mu = 2)
 z <- seq(from = -3, to = 3, by = 1)
 inf3 <- gev_influence(z = z, mu = 2, sigma = 2)
 
-test_that("GEV influence for mu and sigm scale with sigma", {
+test_that("GEV influence for mu and sigma scale with sigma", {
   testthat::expect_equal(inf2[, 3:4] * 2, inf3[, 3:4])
+})
+
+# Check that the GEV influence function for mu is identical to that for
+# the 1 / (1 - exp(-1)) return level
+z <- seq(from = -3, to = 3, by = 1)
+m <- c(1 / (1 - exp(-1)), 50, 100)
+infmu <- gev_influence(z = z, mu = 0, sigma = 1, xi = 0.1)
+infrl <- gev_influence_rl(z = z, mu = 0, sigma = 1, xi = 0.1, m = m)
+
+test_that("GEV influence fn for mu = that for correspnding return level", {
+  testthat::expect_equal(infmu[, 3], infrl[, 3])
 })
